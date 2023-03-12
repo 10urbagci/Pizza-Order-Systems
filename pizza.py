8b4072ead5bdb226dbd3a720c4b218cd4ff28ed6
10urbagci <onurbagci2058@gmail.com>
2023-03-12 23:35:02 +0300

    project is finished.

---
+++ pizza.py
@@ -0,0 +1,13 @@
+#Create super class pizza
+class Pizza:
+
+    def __init__(self,description,cost):
+        self.description = description
+        self.cost = cost
+
+    def get_description(self):
+        return self.description
+
+    def get_cost(self):
+        return self.cost
+
